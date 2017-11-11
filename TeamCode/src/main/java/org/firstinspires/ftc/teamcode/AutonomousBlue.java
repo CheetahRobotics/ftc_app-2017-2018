d53282366c8f0557f3a1aa85c6ea349a7cde7adb
Bob Woodley <blwoodley@yahoo.com>
2017-11-11 11:37:57 -0600

    Add autonomous code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ * <p>
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ * <p>
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name = "AutonomousBlue", group = "Linear Opmode")
+public class AutonomousBlue extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive = hardwareMap.get(DcMotor.class, "motor_1");
+        rightDrive = hardwareMap.get(DcMotor.class, "motor_2");
+        double s = runtime.seconds();
+        int state = 0;
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower = 0;
+            double rightPower = 0;
+            s = runtime.seconds();
+
+            if (s < 1.25) {
+                state = 1;
+                leftPower = 1;
+                rightPower = 1;
+            } else if (s < 2.25) {
+                state = 2;
+                leftPower = 1;
+                rightPower = 0;
+            } else if (s < 6) {
+                state = 3;
+                leftPower = 1;
+                rightPower = 1;
+            } else if (s < 6.15) {
+                state = 4;
+                leftPower = 0;
+                rightPower = 0;
+            }
+
+            // Send calculated power to wheels
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)");//, leftPower, rightPower);
+            telemetry.addData("Name", "Hi my name is Wall-E");
+            telemetry.addData("state", state);
+            telemetry.update();
+
+        }
+    }
+}
